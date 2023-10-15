86a1c6528fca8c265aa5d42d53adc7fc296c2a8d
Lampirg <63813365+Lampirg@users.noreply.github.com>
2023-10-15 17:02:12 +0300

    Update StringHandlerTest.java

--- src/test/java/StringHandlerTest.java
+++ src/test/java/StringHandlerTest.java
@@ -15,20 +15,22 @@ class StringHandlerTest {
     void givenSimpleStream() {
         List<String> input = List.of(
                 "\"111\";\"123\";\"222\"",
-                "\"200\";\"123\";\"100\"",
                 "\"300\";\"\";\"100\"",
+                "\"200\";\"123\";\"100\"",
                 "\"1\";\"1\"",
                 "\"79855053897\"83100000580443402\";\"200000133000191\"",
-                "\"79076513686\""
+                "\"79076513686\"",
+                "\"\";\"\""
         );
         List<Group> expected = List.of(
                 Group.from(List.of(
                         Line.line(List.of(of(111, 0), of(123, 1), of(222, 2))),
                         Line.line(List.of(of(200, 0), of(123, 1), of(100, 2))),
-                        Line.line(List.of(of(300, 0), of(-1, 1), of(100, 2)))
+                        Line.line(List.of(of(300, 0), empty(1), of(100, 2)))
                 )),
                 Group.fromSingleValue(Line.line(List.of(of(1, 0), of(1, 1)))),
-                Group.fromSingleValue(Line.line(List.of(of(79076513686L, 0))))
+                Group.fromSingleValue(Line.line(List.of(of(79076513686L, 0)))),
+                Group.fromSingleValue(Line.line(List.of(empty(0), empty(1))))
         );
         List<Group> actual = StringHandler.getStat(input::stream);
         Assertions.assertThat(actual).hasSameElementsAs(expected);
